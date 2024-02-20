1d631e506ad8fd3bf5a22dfbd8329c6e822294bb
HotNoob <ewbobman@gmail.com>
2024-02-20 09:50:26 -0600

    Update growatt2mqtt.py

--- growatt2mqtt.py
+++ growatt2mqtt.py
@@ -186,7 +186,7 @@ class Growatt2MQTT:
                     json_object = json.dumps(points[0], indent=4)
 
                     self.__mqtt_client.publish(self.__mqtt_topic, json_object, 0, properties=self.__properties)
-                    self.__mqtt_client.publish(self.__mqtt_topic+'/OP_Watt', str(info['OP WATT']), 0, properties=self.__properties)
+                    self.__mqtt_client.publish(self.__mqtt_topic+'/OP_Watt', str(info['OP WATT']))
 
                 except Exception as err:
                     traceback.print_exc()
