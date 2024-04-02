e1a5ef341e76f8a6d31389696ced909b846624e6
HotNoob <ewbobman@gmail.com>
2024-04-01 19:02:04 -0500

    Update transport_base.py

--- classes/transports/transport_base.py
+++ classes/transports/transport_base.py
@@ -51,7 +51,7 @@ class transport_base:
         if settings:
             self.device_serial_number = settings.get("serial_number", self.device_serial_number)
             self.device_manufacturer = settings.get("manufacturer", self.device_manufacturer)
-            self.device_name = settings.get("name", fallback=self.device_manufacturer+" "+self.device_name)
+            self.device_name = settings.get("device_name", fallback=self.device_manufacturer+" "+self.device_name)
             self.bridge = settings.get("bridge", self.bridge)
             self.read_interval = settings.getfloat("read_interval", self.read_interval)
             self.write_enabled = settings.getboolean("write_enabled", self.write_enabled)
