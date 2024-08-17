85f903d21bf5a677b6d2404d0462732bf8e3f684
HotNoob <ewbobman@gmail.com>
2024-08-17 13:42:35 -0500

    folder organization for protocols

--- pytests/test_protocol_settings.py
+++ pytests/test_protocol_settings.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import pytest
+import glob
+
 
 #move up a folder for tests
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
@@ -8,11 +10,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from classes.protocol_settings import protocol_settings
 
 # List of protocols to test
-protocols = [os.path.splitext(f)[0] for f in os.listdir("protocols") if f.endswith('.json')]
+# Create the search pattern to find .json files recursively
+search_pattern = os.path.join("protocols", '**', '*.json')
+# Use glob to find all files matching the pattern
+files = glob.glob(search_pattern, recursive=True)
+# Extract file names without extension
+protocols = [os.path.splitext(os.path.basename(f))[0] for f in files]
 
 
 # Parameterized test function
 @pytest.mark.parametrize("protocol", protocols)
 def test_protocol_setting(protocol : str):
+    print(protocol)
     protocolSettings : protocol_settings = protocol_settings(protocol)
 
